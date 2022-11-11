21e2b479b4cab05a782d8756c98e5c75c2a24a7e
barakbbn <barakbbn@gmail.com>
2022-11-11 19:17:04 +0200

    Adjusted files location for Nuget

--- WpfMart/Properties/AssemblyInfo.cs
+++ WpfMart/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Windows.Markup;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WpfMart")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WPF helpers and utilities, Converter, Markup-extension")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfMart")]
@@ -52,8 +52,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
 [assembly:InternalsVisibleTo("ConvertersPoC")]
 [assembly:InternalsVisibleTo("WpfMart.Tests")]
--- WpfMartSamples/Properties/AssemblyInfo.cs
+++ WpfMartSamples/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfMartSamples")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
